c7c5886d4f37f493e694eb506be1d9a0c816541c
grind-t <grindt.gd@gmail.com>
2021-09-08 20:36:50 +0300

    add reviews section

---
+++ components/organisms/reviews-section.jsx
@@ -0,0 +1,34 @@
+import styles from "./reviews-section.module.css";
+import Image from "next/image";
+import ReviewSlider from "../molecules/review-slider";
+
+const ReviewsSection = () => {
+  const review = {
+    heading: "Мой лучший опыт",
+    text: "Я всегда пытаюсь выйти за рамки и выбрать прическу, которую вы не увидите ни на ком другом. У меня действительно хорошие волосы, и я могу делать с ним много разных вещей.",
+    author: {
+      image: "/images/review-author1.jpeg",
+      name: "Василий Юрьев",
+      profession: "модель",
+    },
+  };
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.leftDecoration}>
+        <Image
+          src="/images/happy-customer.jpeg"
+          alt=""
+          layout="fill"
+          objectFit="cover"
+          objectPosition="left top"
+        />
+      </div>
+      <div className={styles.overlay}>
+        <ReviewSlider reviews={[review, review]} headingLevel="h2" />
+      </div>
+    </section>
+  );
+};
+
+export default ReviewsSection;
