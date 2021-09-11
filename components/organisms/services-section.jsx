9fab7be76d0b060a23b1ed95d7e1adfc9d366501
grind-t <grindt.gd@gmail.com>
2021-09-11 14:19:16 +0300

    add services section

---
+++ components/organisms/services-section.jsx
@@ -0,0 +1,45 @@
+import styles from "./services-section.module.css";
+import Link from "next/link";
+
+const ServicesSection = () => (
+  <section className={styles.container}>
+    <h2 className={styles.heading}>Прейскурант</h2>
+    <div className={styles.content}>
+      <ul className={styles.services}>
+        <li>
+          <h3 className={styles.serviceHeading}>Мужская стрижка</h3>
+          <strong className={styles.servicePrice}>600 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>
+            Стрижка машинкой одна насадка
+          </h3>
+          <strong className={styles.servicePrice}>300 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>Моделирование бороды</h3>
+          <strong className={styles.servicePrice}>500 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>Стрижка + борода</h3>
+          <strong className={styles.servicePrice}>1100 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>Опасное бритье</h3>
+          <strong className={styles.servicePrice}>600 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>Детская стрижка</h3>
+          <strong className={styles.servicePrice}>400 Р</strong>
+        </li>
+        <li>
+          <h3 className={styles.serviceHeading}>Эпиляция воском</h3>
+          <strong className={styles.servicePrice}>200 Р</strong>
+        </li>
+      </ul>
+      <button className={styles.bookingButton}>Записаться сейчас!</button>
+    </div>
+  </section>
+);
+
+export default ServicesSection;
